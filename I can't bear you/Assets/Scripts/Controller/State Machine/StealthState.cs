0e8cb4ba8725930b892d204a739af0f301d89c3a
Gautier DEBREU <g.debreu@rubika-edu.com>
2022-10-14 15:37:25 +0200

    Beginning of detection system

--- I can't bear you/Assets/Scripts/Controller/State Machine/StealthState.cs
+++ I can't bear you/Assets/Scripts/Controller/State Machine/StealthState.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class StealthState : PlayerState
 {
     [SerializeField] private PlayerState bearserkerState;
+    [SerializeField] private float detectionAngle, detectionRange;
     public override void Behave()
     {
         if (inputManager.interactDown)
@@ -21,18 +22,41 @@ public class StealthState : PlayerState
     public override void FixedBehave()
     {
         Move();
+        LookForInteractables();
     }
+    
     public void LookForInteractables()
     {
-        Debug.Log(transform.forward);
-        Debug.DrawRay(transform.position, transform.forward * 2, Color.red);
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 2f))
+        for (int i = 0; i < detectionAngle; i++)
         {
-            /*if (hit.collider.gameObject.GetComponent<Interactable>())
-            {
-                hit.collider.gameObject.GetComponent<Interactable>().
-            }*/
+            Debug.Log(GetVectorFromAngle(0.5f));
+            SendRayCast(transform.position,
+                (transform.forward + new Vector3(0.1f,0,0.1f)*i).normalized,
+                    detectionRange);
+            SendRayCast(transform.position, (transform.forward + new Vector3(0.1f,0,0.1f)*i).normalized, detectionRange);
         }
+        
+    }
+
+    private bool SendRayCast(Vector3 origin, Vector3 dir, float length)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(origin, dir, out hit, length))
+        {
+            if (hit.collider.gameObject.GetComponent<IInteractable>() != default)
+            {
+                //hit.collider.gameObject.GetComponent<IInteractable>().Interact();
+                Debug.DrawRay(origin, dir * hit.distance, Color.red);
+                return true;
+            }
+            Debug.DrawRay(origin, dir * length, Color.green);
+            return false;
+        }
+        Debug.DrawRay(origin, dir * length, Color.green);
+        return false;
+    }
+    public static Vector3 GetVectorFromAngle(float angle) {
+        float angleRad = angle * (Mathf.PI/180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
     }
 }
