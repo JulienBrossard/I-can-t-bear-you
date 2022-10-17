dd82e8712c1cf08a6aa4ce875e36375e08981305
Julien BROSSARD <j.brossard@rubika-edu.com>
2022-10-17 14:11:41 +0200

    Added npc data as sciptable object

---
+++ I can't bear you/Assets/Scripts/NpcData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Npc data", menuName = "Npc Data")]
+public class NpcData : ScriptableObject
+{
+    public float maxHunger;
+    public float maxThirst;
+    public float maxBladder;
+    public float speed = 10;
+}
