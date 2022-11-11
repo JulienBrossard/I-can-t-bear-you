ed1370ddb71c13cd37405f1d5301947a42419016
Gautier DEBREU <g.debreu@rubika-edu.com>
2022-11-11 16:46:22 +0100

    Puddle Class

--- I can't bear you/Assets/Scripts/Interactables/Items/Puddles/Puddle.cs
+++ I can't bear you/Assets/Scripts/Interactables/Items/Puddles/Puddle.cs
@@ -3,9 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Puddle : MonoBehaviour
+public class Puddle : MonoBehaviour
 {
-    protected abstract void ApplyEffects(GameObject go);
+    public bool ignitable, ignited, conductor,charged, acid, slippy, sticky;
+
+    public void ApplyEffects(GameObject go)
+    {
+        if(ignited) go.GetComponent<Entity>()?.Ignite();
+        if(charged) go.GetComponent<Entity>()?.Electrocute();
+        if(acid) go.GetComponent<Entity>()?.Dissolve();
+        if(slippy) go.GetComponent<Entity>()?.Slide();
+        if(sticky) go.GetComponent<Entity>()?.Slow();
+    }
 
     protected void OnTriggerEnter(Collider other)
     {
