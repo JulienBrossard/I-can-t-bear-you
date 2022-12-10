fcf0f5ac4f926031a0afbfcaecee6132ea09cf7f
Anthime <anthime.roux@hotmail.fr>
2022-12-10 21:38:08 +0100

    Wall updater script finished

--- I can't bear you/Assets/Scripts/WallUpdater.cs
+++ I can't bear you/Assets/Scripts/WallUpdater.cs
@@ -1,55 +1,64 @@
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 
 public class WallUpdater : MonoBehaviour
 {
     private Renderer objRenderer;
-    [SerializeField] private Material[] transparentMaterials;
+    private Material[] transparentMaterials;
     [SerializeField] private bool wallInRoomYouStart = false;
-    private Material[] opaqueMaterials;
-
+    [SerializeField]private Material[] opaqueMaterials;
+    [SerializeField] private float timeToFade = 1f;
     private void Start()
     {
         objRenderer = gameObject.GetComponent<Renderer>();
-        opaqueMaterials = objRenderer.materials;
+        transparentMaterials = objRenderer.materials;
         if (wallInRoomYouStart)
         {
             HideWalls();
         }
+        else
+        {
+            SwitchToOpaqueMaterials();
+        }
     }
     public void ShowWalls()
     {
-        for (int x = 0; x < opaqueMaterials.Length; x++)
+        foreach (var mat in transparentMaterials)
         {
-            transparentMaterials[x].DOFade(1, 0.1f);
+            mat.DOFade(0, 0f);
+            mat.DOFade(1, timeToFade);
         }
+
+        StartCoroutine(WaitBeforeSwitchingToOpaqueMaterials());
     }
     
     public void HideWalls()
     {
+        Debug.Log("HideWalls");
         SwitchToTransparentMaterials();
         foreach (var mat in transparentMaterials)
         {
-            mat.DOFade(0, 0.1f).OnComplete (() => SwitchToOpaqueMaterials());
+//            mat.DOFade(1, 0f);
+            mat.DOFade(0, timeToFade);
         }
     }
     
     
     private void SwitchToTransparentMaterials()
     {
-        for (int i = 0; i < objRenderer.materials.Length; i++)
-        {
-            objRenderer.materials[i] = transparentMaterials[i];
-        }
+        objRenderer.materials = transparentMaterials;
     }
     
     
     private void SwitchToOpaqueMaterials()
     {
-        for (int i = 0; i < objRenderer.materials.Length; i++)
-        {
-            objRenderer.materials[i] = opaqueMaterials[i];
-        }
+        objRenderer.materials = opaqueMaterials;
     }
 
+    IEnumerator WaitBeforeSwitchingToOpaqueMaterials()
+    {
+        yield return new WaitForSeconds(timeToFade);
+        SwitchToOpaqueMaterials();
+    }
 }
