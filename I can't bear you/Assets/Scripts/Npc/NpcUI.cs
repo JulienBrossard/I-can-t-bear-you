e2a1ad698bf9a3c5dbe1c0c953f77e1f07bbdb56
Anthime ROUX <anthime.roux@hotmail.fr>
2023-01-19 17:41:50 +0100

    tv icon begin

--- I can't bear you/Assets/Scripts/Npc/NpcUI.cs
+++ I can't bear you/Assets/Scripts/Npc/NpcUI.cs
@@ -10,6 +10,7 @@ public class NpcUI : MonoBehaviour
     public GameObject thirstImage;
     public GameObject hungerImage;
     public GameObject bladderImage;
+    public GameObject tvImage;
     
     [Header("Panic Feedback")] 
     public Image suspiciousImage;
